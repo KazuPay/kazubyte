e07f486b42dd3ed9aac4aa2575c575996d555327
Kazucoin <45504088+kazucoin@users.noreply.github.com>
2019-02-22 07:50:08 +0000

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -21,14 +21,13 @@ extern const std::string CURRENCY_UNIT;
 
 /** No amount larger than this (in satoshi) is valid.
  *
- * Note that this constant is *not* the total money supply, which in KazuByte
- * currently happens to be less than 84,000,000 KBYTE for various reasons, but
+ * Note that this constant is *not* the total money supply, but
  * rather a sanity check. As this sanity check is used by consensus-critical
  * validation code, the exact value of the MAX_MONEY constant is consensus
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 84000000 * COIN;
+static const CAmount MAX_MONEY = 8400000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
